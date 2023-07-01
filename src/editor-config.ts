c4899586279773f9a3476ba8111bdd78646362fc
Linh pham <pnlinh.it@gmail.com>
2023-07-01 08:58:38 +0700

    Update toolbar

--- src/editor-config.ts
+++ src/editor-config.ts
@@ -7,17 +7,30 @@ const config: EditorOptions & {
 } = {
   lists_indent_on_tab: true,
   placeholder: 'Empty',
-  branding: false,
   // toolbar: 'undo redo bold italic underline bullist code',
-  // toolbar: [],
   plugins:
     'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
-  toolbar:
+
+  // https://www.tiny.cloud/docs/tinymce/6/toolbar-configuration-options/#link_context_toolbar
+  toolbar: [],
+  toolbar1:
     'undo redo | fontsizeinput hr lineheight newdocument remove selectall styles subscript superscript visualaid | paste pastetext cut | bold italic underline strikethrough blockquote | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl | help',
+  toolbar2: 'alignleft aligncenter alignright',
+  toolbar_mode: 'wrap',
   inline: false,
   menubar: false,
   toolbar_sticky: true,
   // toolbar_sticky_offset: 108,
+  link_context_toolbar: true,
+
+  quickbars_insert_toolbar: 'quickimage quicktable | hr pagebreak',
+  quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+  quickbars_image_toolbar:
+    'alignleft aligncenter alignright | rotateleft rotateright | imageoptions',
+
+  table_toolbar:
+    'tableprops tabledelete | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol',
+
   autosave_ask_before_unload: true,
   autosave_interval: '30s',
   autosave_prefix: '{path}{query}-{id}-',
@@ -55,11 +68,13 @@ const config: EditorOptions & {
   },
   height: 600,
   image_caption: true,
-  quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
   noneditable_class: 'mceNonEditable',
-  toolbar_mode: 'sliding',
   contextmenu: 'link image table',
+  statusbar: false,
   resize: false,
+  elementpath: false,
+  branding: false,
+  highlight_on_focus: true,
   // width: '100px',
   // max_height: '100px',
   // Don't work with blockquote
